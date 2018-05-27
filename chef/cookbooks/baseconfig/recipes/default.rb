a21bc921079ae5cba6b78f5008f8ddda3b4e1c43
ximin zhang <ximinzhang@d142-058-163-053.wireless.sfu.ca>
2018-05-27 16:29:53 -0700

    working for now

--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -9,9 +9,25 @@ end
 # Base configuration recipe in Chef.
 package "wget"
 package "ntp"
+package "nginx"
+package "postgresql"
+
 cookbook_file "ntp.conf" do
   path "/etc/ntp.conf"
 end
+
+cookbook_file "nginx.default" do
+    path "/etc/nginx/sites-available/default"
+end
+
 execute 'ntp_restart' do
   command 'service ntp restart'
+end
+
+execute 'nginx_restart' do
+  command 'sudo systemctl restart nginx'
+end
+
+execute 'postgresql_create' do
+	command 'echo "CREATE DATABASE mydb; CREATE USER vagrant; GRANT ALL PRIVILEGES ON DATABASE mydb TO vagrant;" | sudo -u postgres psql'
 end
