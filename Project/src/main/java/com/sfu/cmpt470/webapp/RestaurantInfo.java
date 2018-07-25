d80864e7c6c96cb93f679a7db1032dcb8ad0fcdc
hqiu <hqiu@sfu.ca>
2018-07-24 20:07:05 -0700

    fix schema name

--- Project/src/main/java/com/sfu/cmpt470/webapp/RestaurantInfo.java
+++ Project/src/main/java/com/sfu/cmpt470/webapp/RestaurantInfo.java
@@ -3,10 +3,7 @@ package com.sfu.cmpt470.webapp;
 import com.sfu.cmpt470.service.RestInfoService;
 import com.sfu.cmpt470.service.RestInfoServiceImpl;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -14,9 +11,10 @@ import javax.ws.rs.core.Response;
  * Created by Administrator on 7/17/2018.
  */
 
-@Path("/info/{restaurant_id}")
+@Path("/restaurant")
 public class RestaurantInfo {
     //option1: rest/info?restaurant_id={restId}
+    @Path("/info/{restaurant_id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response openInfo(@PathParam("restaurant_id") int restaurantID){
@@ -26,7 +24,29 @@ public class RestaurantInfo {
             return Response.ok().entity(result).build();
         }catch (Exception e){
             e.printStackTrace();
-            return Response.status(401).entity("Server can not process the request").build();
+            return Response.status(401).entity("Server can not process request").build();
+        }
+    }
+
+    @Path("/reserve")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response reserveTable(String reservation){
+        try{
+            if(reservation != null){
+                System.out.println(reservation);
+                return Response.ok("Your reservation is booked.").build();
+            }else{
+                throw new NullPointerException();
+            }
+
+
+        }catch (NullPointerException nex){
+            return Response.status(400).entity("JSON cannot be empty").build();
+        }catch(Exception e){
+            e.printStackTrace();
+            return Response.status(400).entity("Server can not post request").build();
         }
     }
 
